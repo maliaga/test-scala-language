776719d8345b0d4f2e4dbdadd8773da4e2ff04d4
Mario Aliaga <ma@marioaliaga.com>
2017-07-25 22:05:27 -0400

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "test-languaje"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+        
---
+++ src/main/scala/com/marioaliaga/app/HelloWorld.scala
@@ -0,0 +1,10 @@
+package com.marioaliaga.app
+
+/**
+  * Created by maliaga on 7/25/17.
+  */
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hi Mario Aliaga")
+  }
+}
